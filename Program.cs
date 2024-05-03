d51598faaf4ddc4eb6a48d3fd035d6e429b4bd77
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-05-02 21:26:12 -0300

    Adjust code to always read the appsettings.json file on each iteration in while

--- Program.cs
+++ Program.cs
@@ -10,16 +10,15 @@ namespace AutoRefreshHDR
         {
             try
             {
-                var configFilePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
-
-                DisplayConfig displayConfig = JsonConvert.DeserializeObject<DisplayConfig>(File.ReadAllText(configFilePath)) ?? new DisplayConfig();
-
                 bool hdrActivated = false;
                 bool refreshRateChange = false;
 
                 Console.WriteLine("Checking for the execution of any program in the list...");
                 while (true)
                 {
+                    var configFilePath = Path.Combine(AppContext.BaseDirectory, "appsettings.json");
+                    DisplayConfig displayConfig = JsonConvert.DeserializeObject<DisplayConfig>(File.ReadAllText(configFilePath)) ?? new DisplayConfig();
+
                     foreach (var programDisplayConfig in displayConfig.ProgramDisplayConfigs)
                     {
                         if (Process.GetProcessesByName(programDisplayConfig.ProgramName.Replace(".exe", "")).Length != 0)
