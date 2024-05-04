d8f8f07c4c6e10d1235be1c4628e52af52e31830
Weverton Pires <30535993+magpires@users.noreply.github.com>
2024-05-04 16:46:00 -0300

    Update ProgramDisplayConfig.cs

--- Models/ProgramDisplayConfig.cs
+++ Models/ProgramDisplayConfig.cs
@@ -8,7 +8,7 @@ namespace AutoRefreshHDR.Models
 {
     public class ProgramDisplayConfig
     {
-        public string ProgramName { get; set; }
+        public string ProgramName { get; set; } = string.Empty;
         public int refreshRate { get; set; }
         public bool Hdr { get; set; }
     }
