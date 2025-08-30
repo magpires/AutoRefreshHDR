aaa4a6d7b7a0c00db490ad826c6636bddb9c780e
Weverton Pires <30535993+magpires@users.noreply.github.com>
2025-08-29 21:21:02 -0300

    perf: Use uint instead of int

--- Models/ProgramDisplayConfig.cs
+++ Models/ProgramDisplayConfig.cs
@@ -3,7 +3,7 @@
     public class ProgramDisplayConfig
     {
         public string ProgramName { get; set; } = string.Empty;
-        public int RefreshRate { get; set; }
+        public uint RefreshRate { get; set; }
         public bool Hdr { get; set; }
     }
 }
