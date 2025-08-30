42f37485dcbfc54f56ecab71d8aad0d7a9d7b5fd
Weverton Pires <30535993+magpires@users.noreply.github.com>
2025-08-29 22:43:52 -0300

    feat: Add BrightnessLevel and Active in ProgramDisplayConfig

--- Models/ProgramDisplayConfig.cs
+++ Models/ProgramDisplayConfig.cs
@@ -1,9 +1,11 @@
 ﻿namespace AutoRefreshHDR.Models
 {
-    public class ProgramDisplayConfig
+    public abstract class ProgramDisplayConfig
     {
         public string ProgramName { get; set; } = string.Empty;
         public uint RefreshRate { get; set; }
         public bool Hdr { get; set; }
+        public uint BrightnessLevel { get; set; }
+        public bool Active { get; init; }
     }
 }
