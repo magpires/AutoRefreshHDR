c8ae11a53e39680035e188c47f30a182962495fa
Weverton Pires <30535993+magpires@users.noreply.github.com>
2025-12-06 00:34:38 -0300

    refactor: Reestrutura projeto

--- KillAutoRefreshHDR.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-
-echo Killing AutoRefreshHDR...
-taskkill /IM AutoRefreshHDR.exe /F
-timeout /t 2 >nul
-exit
--- RestartAutoRefreshHDR.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-taskkill /IM AutoRefreshHDR.exe /F
-echo Restarting AutoRefreshHDR...
-timeout /t 2 >nul
-start "" "AutoRefreshHDR.exe"
-exit
