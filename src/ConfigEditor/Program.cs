5f7a60341f5cd8fa175859c945365aafa2a0cdff
Weverton Pires <30535993+magpires@users.noreply.github.com>
2025-12-07 18:23:11 -0300

    refactor: Transforma o método main em private

--- src/ConfigEditor/Program.cs
+++ src/ConfigEditor/Program.cs
@@ -9,7 +9,7 @@ static class Program
     ///  The main entry point for the application.
     /// </summary>
     [STAThread]
-    static void Main()
+    private static void Main()
     {
         ApplicationConfiguration.Initialize();
         Application.Run(new ConfigEditorForm());
