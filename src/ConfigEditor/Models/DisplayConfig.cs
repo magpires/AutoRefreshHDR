6e495b1b8c1e42f8fea6e2e38bb944c9d8bb62b1
Weverton Pires <30535993+magpires@users.noreply.github.com>
2025-12-07 18:37:57 -0300

    refactor: Separa as classes em arquivos diferentes

--- src/ConfigEditor/Models/DisplayConfig.cs
+++ src/ConfigEditor/Models/DisplayConfig.cs
@@ -10,16 +10,3 @@ public class DisplayConfig
 
     public bool UseBrightnessLevel { get; set; }
 }
-
-public class ProgramDisplayConfig
-{
-    public string ProgramName { get; set; } = string.Empty;
-
-    public uint? RefreshRate { get; set; }
-
-    public bool Hdr { get; set; }
-
-    public uint? BrightnessLevel { get; set; }
-
-    public bool Active { get; set; }
-}
---
+++ src/ConfigEditor/Models/ProgramDisplayConfig.cs
@@ -0,0 +1,11 @@
+﻿namespace ConfigEditor.Models
+{
+    public class ProgramDisplayConfig
+    {
+        public string ProgramName { get; init; } = string.Empty;
+        public uint? RefreshRate { get; init; }
+        public bool Hdr { get; init; }
+        public uint? BrightnessLevel { get; init; }
+        public bool Active { get; init; }
+    }
+}
